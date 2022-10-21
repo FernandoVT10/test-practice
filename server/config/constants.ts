422f7d55a6bc8d0019e23154df75254e6947c3d3
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2022-10-21 18:49:24 -0500

    Fixing constant type

--- server/config/constants.ts
+++ server/config/constants.ts
@@ -7,4 +7,4 @@ export const PRODUCTION = process.env.NODE_ENV === "production";
 export const ROOT_DIR = path.resolve(__dirname, "../../");
 export const LOGFILES_DIR = path.resolve(ROOT_DIR, "logs");
 
-export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY as string;
