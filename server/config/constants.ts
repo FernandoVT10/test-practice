40305d704c1e61889e680f254e9691e12f29f81f
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2022-10-21 18:48:10 -0500

    Installed dotenv to use .env files

--- server/config/constants.ts
+++ server/config/constants.ts
@@ -1,8 +1,10 @@
 import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const PRODUCTION = process.env.NODE_ENV === "production";
 
 export const ROOT_DIR = path.resolve(__dirname, "../../");
 export const LOGFILES_DIR = path.resolve(ROOT_DIR, "logs");
 
-export const JWT_SECRET_KEY = "shhh";
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
