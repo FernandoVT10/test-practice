e186985c1c6f8e4aed98b3324d61b3b9ee31a9d1
Fernando Vaca Tamayo <vacatamayo321@gmail.com>
2022-11-23 12:36:25 -0600

    New commands to work with cypress

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
-const { PHASE_PRODUCTION_BUILD } = require("next/constants");
+const { PHASE_PRODUCTION_BUILD, PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -17,7 +17,8 @@ const nextConfig = {
 };
 
 module.exports = (phase) => {
-  if(phase === PHASE_PRODUCTION_BUILD && process.env.NODE_ENV === "test") {
+  if(phase === PHASE_PRODUCTION_BUILD && process.env.BUILD_TYPE === "test"
+    || phase === PHASE_DEVELOPMENT_SERVER) {
     // don't get rid of data-test attributes for testing with cypress
     nextConfig.compiler.reactRemoveProperties = false;
   }
